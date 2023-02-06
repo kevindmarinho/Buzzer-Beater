275e403224a9fe11fdc5ad2fef7867037dbcb126
kevin marinho <kevindmarinho4321@gmail.com>
2023-02-06 18:44:11 -0400

    Initial Commit

---
+++ BuzzerBeater/BuzzerBeaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzerBeaterApp.swift
+//  BuzzerBeater
+//
+//  Created by kevin marinho on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BuzzerBeaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
